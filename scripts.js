bd7d6ee2f58bae24c5fd818137df32206e947f95
047juniorr <gledson.enterprise@gmail.com>
2024-04-20 16:04:21 -0300

    Deploy projeto

---
+++ scripts.js
@@ -0,0 +1,20 @@
+function colocarNaTela(dados){
+    document.querySelector('.cidade').innerHTML=' Tempo em ' + dados.name
+    document.querySelector('.temp').innerHTML= Math.floor(dados.main.temp) + 'ºC'
+    document.querySelector('.icone').src = 'https://openweathermap.org/img/wn/' + dados.weather[0].icon + '.png'
+    document.querySelector('.umidade').innerHTML = 'Máxima de ' + dados.main.temp_max + 'ºC' + ' e Mínimas de ' + dados.main.temp_min + 'ºC'
+    console.log(dados)
+}
+
+async function buscarCidade(cidade) {
+    let dados = await fetch('https://api.openweathermap.org/data/2.5/weather?q=' + cidade +'&appid=cebcd482eda57fa9a6714c1c2ba91885&units=metric').then(response => response.json())
+
+    colocarNaTela(dados)
+
+}
+function CliqueiNoBotao() {
+    let cidade = document.querySelector(".input-cidade").value;
+
+    buscarCidade(cidade)
+    
+}
